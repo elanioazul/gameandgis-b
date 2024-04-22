29c97fff0fb60046c8eac66713f4844f9cb7d321
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-22 17:02:45 +0200

    make public (AythType.none) sign in and sign up routes

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -10,6 +10,7 @@ import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
 
 @Module({
   imports: [
@@ -24,8 +25,9 @@ import { APP_GUARD } from '@nestjs/core';
     },
     {
       provide: APP_GUARD,
-      useClass: AccessTokenGuard,
+      useClass: AuthenticationGuard,
     },
+    AccessTokenGuard,
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
