7cb30248e5ed407cf3b34e1cb184511264c43e35
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-22 16:12:17 +0200

    protect all routes wiht Guard and logs decoded user payload

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -8,6 +8,8 @@ import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -19,6 +21,10 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: HashingService,
       useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
     },
     AuthenticationService,
   ],
