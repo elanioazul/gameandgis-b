34ca1af7001fc288ca93ad4ef9208261479edf9f
hugo <hugo.herrador@gmail.com>
2024-06-01 13:37:40 +0200

    Roleseeder ensure roles are create in db for new created users default value

---
+++ src/roles/classes/roleseeder/roleseeder.spec.ts
@@ -0,0 +1,7 @@
+import { Roleseeder } from './roleseeder';
+
+describe('Roleseeder', () => {
+  it('should be defined', () => {
+    expect(new Roleseeder()).toBeDefined();
+  });
+});
---
+++ src/roles/classes/roleseeder/roleseeder.ts
@@ -0,0 +1,28 @@
+import { OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Role } from 'src/roles/entities/role.entity';
+import { Repository } from 'typeorm';
+
+export class Roleseeder implements OnModuleInit {
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+  ) {}
+
+  async onModuleInit() {
+    await this.seed();
+  }
+
+  async seed() {
+    const roles = ['regular', 'admin']; // Add other roles as needed
+    for (const roleName of roles) {
+      const role = await this.roleRepository.findOne({
+        where: { name: roleName },
+      });
+      if (!role) {
+        const newRole = this.roleRepository.create({ name: roleName });
+        await this.roleRepository.save(newRole);
+      }
+    }
+  }
+}
